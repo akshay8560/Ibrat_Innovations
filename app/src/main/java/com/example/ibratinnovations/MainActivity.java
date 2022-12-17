f1bd62d877e8a3205e0523cc45770350262e6747
akshaykumar <doodwalakshay@gmail.com>
2022-12-17 16:40:38 +0530

    New Commit

---
+++ app/src/main/java/com/example/ibratinnovations/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ibratinnovations;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+
+        Intent intent = new Intent(MainActivity.this, StartGame.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
